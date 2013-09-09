1fa91a636524dbb84d3993505a61175ade6da570
Naomi Sorbet <naomisorbet@gmail.com>
2013-09-09 11:03:11 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FriendsCircles::Application.config.secret_token = '1a910b8f33bf0a729fb40d7d82eab4588becc7f44b2b33063fd96f34b50b120ad10b59e88e275c1bdd0c51c8b4c87afee297eeb89e2b4a26fc92ad0c10f239f7'
